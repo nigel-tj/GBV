2298979d6edc4ef770509aaf4fd69141f0261a7a
Nigel <hypej.10@gmail.com>
2020-03-09 23:49:11 +0200

    add map

--- src/app/pages/notifications/notifications.page.ts
+++ src/app/pages/notifications/notifications.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Map, latLng, tileLayer, Layer, marker } from 'leaflet';
+
 
 @Component({
   selector: 'app-notifications',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notifications.page.scss'],
 })
 export class NotificationsPage implements OnInit {
+  map: Map;
+  ionViewDidEnter() { this.leafletMap(); }
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+ leafletMap() {
+    // In setView add latLng and zoom
+    this.map = new Map('mapId').setView([28.644800, 77.216721], 10);
+    tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+      attribution: 'edupala.com © ionic LeafLet',
+    }).addTo(this.map);
+
+
+    marker([28.6, 77]).addTo(this.map)
+      .bindPopup('help centre info')
+      .openPopup();
+  }
+
+  /** Remove map when we have multiple map object */
+  ionViewWillLeave() {
+    this.map.remove();
   }
 
 }
