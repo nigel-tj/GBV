73ec9f75513e2bfe14bc9cf2efda154cb9ddadff
Nigel <hypej.10@gmail.com>
2020-03-09 22:28:41 +0200

    add authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,35 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//firebase
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AuthenticationService } from './services/authentication.service';
+import * as firebase from 'firebase';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// environment
+import { environment } from '../environments/environment';
+firebase.initializeApp(environment.firebaseConfig);
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,
+  	   IonicModule.forRoot(),
+	   AppRoutingModule,
+	   AngularFireModule,
+    	   AngularFireAuthModule,
+	   AngularFireDatabaseModule,
+    	   AngularFireStorageModule,
+	   ReactiveFormsModule
+	   ],
   providers: [
     StatusBar,
     SplashScreen,
+    AuthenticationService,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,21 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCcgIFxX8VE8cONPSCvE9ylhhAaK1sT4bs",
+    authDomain: "ionic-92142.firebaseapp.com",
+    databaseURL: "https://ionic-92142.firebaseio.com",
+    projectId: "ionic-92142",
+    storageBucket: "ionic-92142.appspot.com",
+    messagingSenderId: "214578467644",
+    appId: "1:214578467644:web:2c95d6bb23051523540439",
+    measurementId: "G-3BEDB2LTV1"
+
+  }
 };
+
+
 
 /*
  * For easier debugging in development mode, you can import the following file
