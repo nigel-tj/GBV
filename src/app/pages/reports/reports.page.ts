223555f3264421ed0e6103f040a1c07d79b1479a
Nigel Jena <nigel@webbox.co.za>
2020-03-10 18:29:42 +0200

    add report form

--- src/app/pages/reports/reports.page.ts
+++ src/app/pages/reports/reports.page.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-reports',
   templateUrl: './reports.page.html',
   styleUrls: ['./reports.page.scss'],
 })
-export class ReportsPage implements OnInit {
 
-  constructor() { }
+export class ReportsPage implements OnInit {
+  validations_form: FormGroup;
+  errorMessage: string = '';
+  successMessage: string = '';
+
+  constructor(private navCtrl: NavController,
+    private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+  this.validations_form = this.formBuilder.group({
+      name: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]$')
+      ])),
+      number: new FormControl(''),
+      message: newFormControl('')
+    });
+  }
+
+  sendReport(value){
+    this.authService.registerUser(value)
+     .then(res => {
+       console.log(res);
+       this.errorMessage = "";
+       this.successMessage = "Report Sent";
+     }, err => {
+       console.log(err);
+       this.errorMessage = err.message;
+       this.successMessage = "";
+     })
   }
 
 }
